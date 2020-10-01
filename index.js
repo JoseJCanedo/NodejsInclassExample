d5ad1cf761e120dbb96cb7ae7a2c0c068a575ab6
JoseJCanedo <canedo_jose@columbusstate.edu>
2020-10-01 22:01:44 +0000

    mongo

--- index.js
+++ index.js
@@ -2,11 +2,12 @@
 var express = require('express');
 //require body-parser
 var bodyParser = require("body-parser");
+//require mongoose
+var mongoose = require('mongoose');
 //create express object, call express
 var app = express();
 //get port information
 const port = process.env.PORT || 3000;
-
 //tell application to use EJS for templates
 app.set('view engine', 'ejs');
 //make styles public
@@ -14,16 +15,37 @@ app.use(express.static("public"));
 //tell app to use Body parser
 app.use(bodyParser.urlencoded({extended: true}));
 
+
+const Todo = require('./models/todo.model');
+const mongoDB = 'mongodb+srv://testConnection:b8RwqJYgo4hD1xhe@nodetodoexample-iqnde.mongodb.net/test?retryWrites=true&w=majority';
+mongoose.connect(mongoDB);
+mongoose.Promise = global.Promise;
+let db = mongoose.connection;
+db.on('error', console.error.bind(console, "MongoDB connection error:"));
+
 //Couple of items todo
-var tasks = ["make it to class", "take child to daycare"];
+var tasks = [];
 
 //completed items
-var completed = ["extra work"];
+var completed = [];
 
 //get home page /
 app.get('/', function(req, res){
     //return something to home page
-    res.render('index', {tasks: tasks}); //add completed variable to ejs ex {a:a, b:b}
+    Todo.find(function(err, todo){
+        if(err){
+            console.log(err);		
+        }else{		
+            for(i = 0; i< todo.length; i++){		
+                if(todo[i].done){		
+                    completed.push(todo[i].item);		
+                }else{		
+                    tasks.push(todo[i].item);		
+                }		
+            }		
+        }		
+    });		
+     res.render("index", {tasks:tasks, completed:completed});
 });
 
 //add post method /addtask
